e99b1151cd3feeb918b03af8fc264fdff60568a8
yuanzs <yuanzs>
2020-05-09 16:59:36 +0800

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
 	//基本路径
 	baseUrl: './',
 	//输出文件目录
-	outputDir: 'ceaseBuild',
+	outputDir: 'dist',
 	// eslint-loader 是否在保存的时候检查
 	lintOnSave: false,
 	//放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
